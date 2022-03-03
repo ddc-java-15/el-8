7f8f98fce7148395b0bddc9199afadd795987ba6
Cathy Bastareaud <catybastareaud@gmail.com>
2022-03-03 09:04:18 -0700

    Created Repository/ save method and delete method

---
+++ app/src/main/java/edu/cnm/deepdive/el8/service/UserRepository.java
@@ -0,0 +1,66 @@
+package edu.cnm.deepdive.el8.service;
+
+import android.content.Context;
+import androidx.lifecycle.LiveData;
+import androidx.room.Database;
+import edu.cnm.deepdive.el8.model.dao.UserDao;
+import edu.cnm.deepdive.el8.model.entity.User;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import java.util.List;
+
+public class UserRepository {
+
+  private final Context context;
+
+  private final UserDao userDao;
+
+  public UserRepository(Context context) {
+    this.context = context;
+
+    El8Database database = El8Database.getInstance();
+    userDao = database.getUserDao();
+  }
+
+  public LiveData<User> get(long id) {
+
+    return userDao.select(id);
+
+  }
+
+  public LiveData<List<User>> getAll() {
+
+    return userDao.select();
+  }
+
+  public Single<User> save(User user) {
+
+    return (
+        (user.getId() == 0)
+        ? userDao
+            .insert(user)
+            .map((id) -> {
+              user.setId(id);
+              return user;
+            })
+        : userDao
+            .update(user)
+            .map((count) -> user)
+    )
+        .subscribeOn(Schedulers.io());
+
+  }
+
+  public Completable delete(User user) {
+
+    return (
+        (user.getId() == 0)
+        ? Completable.complete()
+        : userDao
+            .delete(user)
+            .ignoreElement()
+    )
+        .subscribeOn(Schedulers.io());
+  }
+}
